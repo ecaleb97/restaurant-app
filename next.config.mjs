a5e8cb9d886e5435ebc93f27c5539c563f9b0a89
ecaleb97 <valley_rob.0c@icloud.com>
2024-04-04 09:47:55 +0200

    Add .env file and components.json, update next.config.mjs, package.json, globals.css, and tailwind.config.ts

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sumorestaurante.com',
+        port: '',
+        pathname: '/wp-content/uploads/2020/05/sumored2.jpg'
+      },
+      {
+        protocol: 'https',
+        hostname: 'sumorestaurante.com',
+        port: '',
+        pathname: '/wp-content/uploads/2020/05/gif400b.gif'
+      },
+    ]
+  }
+};
 
 export default nextConfig;
