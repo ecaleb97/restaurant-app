0d3c84621fbcd27f6095ab9a00721192ff3689d9
ecaleb97 <valley_rob.0c@icloud.com>
2024-04-19 09:55:17 +0200

    Add auth.ts to tsconfig.json and update dependencies in package.json

--- next.config.mjs
+++ next.config.mjs
@@ -3,17 +3,35 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'sumorestaurante.com',
-        port: '',
-        pathname: '/wp-content/uploads/2020/05/sumored2.jpg'
+        protocol: "https",
+        hostname: "sumorestaurante.com",
+        port: "",
+        pathname: "/wp-content/uploads/2020/05/sumored2.jpg"
       },
       {
-        protocol: 'https',
-        hostname: 'sumorestaurante.com',
-        port: '',
-        pathname: '/wp-content/uploads/2020/05/gif400b.gif'
+        protocol: "https",
+        hostname: "sumorestaurante.com",
+        port: "",
+        pathname: "/wp-content/uploads/2020/05/gif400b.gif"
       },
+      {
+        protocol: "https",
+        hostname: "sumorestaurante.com",
+        port: "",
+        pathname: "/wp-content/uploads/2020/05/cuzco2018_117-768x512.jpg"
+      },
+      {
+        protocol: "https",
+        hostname: "sumorestaurante.com",
+        port: "",
+        pathname: "/wp-content/uploads/2020/05/Nigiri_GyuFoie-768x768.jpg"
+      },
+      {
+        protocol: "https",
+        hostname: "sumorestaurante.com",
+        port: "",
+        pathname: "/wp-content/uploads/2020/05/3cuzco-768x512.jpg"
+      }
     ]
   }
 };
